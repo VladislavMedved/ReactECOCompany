c8db651a03af6eb9b89d31d030a0542338b4918a
Vladislav Medved <medved.vladislav@gmail.com>
2019-07-10 17:42:50 +0300

    initialize projects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { inject, observer } from 'mobx-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Person extends React.Component {
+  render() {
+      return (
+          <div className="123">
+              <h1>{this.props.name}</h1>
+              <h2>role: {this.props.role}</h2>
+          </div>
+      );
+  }
 }
 
-export default App;
+@observer
+class Company extends React.Component {
+  render() {
+      return (
+          <React.Fragment>
+              {this.props.persons.map(x => (
+                  <React.Fragment key={x.name}>
+                      <Person name={x.name} role={x.role}/>
+                      <button onClick={() => this.props.onBtnClick(x)}>Promote</button>
+                  </React.Fragment>
+              ))}
+          </React.Fragment>
+      );
+  }
+}
+
+@inject("store")
+@observer
+class App extends React.Component {
+  render() {
+      return (
+          <>
+            <Company persons={this.props.store.persons} onBtnClick={this.props.store.promote}/>
+            <Tasks/>
+          </>
+      );
+  }
+}
+
+@inject("store")
+@observer
+class Tasks extends React.Component {
+    render() {
+        return (
+            <ul style={{ listStyle: "none"}}>
+                {this.props.store.tasks.map((item, index) => (
+                    <li key={uuidv4() + item.taskName}>
+                        <input
+                            type="checkbox"
+                            checked={item.completed}
+                            onChange={() => item.completed = !item.completed}
+                        />
+                        {item.taskName}&nbsp;
+                        <select>
+                            {this.props.store.persons.map(item => (
+                                <option key={uuidv4() + item.name}>{item.name}</option>
+                            ))}
+                        </select>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { observable, computed, autorun, action, transaction } from "mobx";
+import { Provider, inject } from "mobx-react";
 import './index.css';
-import App from './App';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+class ToDoTasksStore {
+    @observable tasks = [];
+    @observable persons = [
+        {
+            name: "Igor",
+            role: "Team Lead"
+        },
+        {
+            name: "Vitalik",
+            role: "Lead Software developer"
+        },
+        {
+            name: "Vlad",
+            role: "Software developer"
+        },
+    ];
+
+    constructor() {
+        autorun(() => console.log(this.report));
+    }
+
+    @computed
+    get report() {
+        if(!this.tasks.length) {
+            return "empty";
+        }
+        return this.tasks.map(x =>  `${x.taskName} completed: ${x.completed} `).join('\n');
+    }
+
+    addTask = (taskName) => {
+        this.tasks.push({
+            taskName: taskName,
+            completed: false,
+            assignee: null
+        })
+    }
+
+    promote = (person) => {
+        this.persons.find((x) => person.name === x.name).role = "Head of Vasiliy Malaschenko";
+    }
+
+    @action
+    setValue(index, name, completed) {
+        this.tasks[index].taskName = name;
+        this.tasks[index].completed = completed;
+    }
+};
+
+
+const toDoTasksStore = window.store = new ToDoTasksStore();
+toDoTasksStore.addTask('fix bug on GPS apply card');
+toDoTasksStore.addTask('fix bug on GPS apply card 34234234');
+
+toDoTasksStore.setValue(0, "copy texts", true);
+
+
+
+ReactDOM.render(
+<Provider store={toDoTasksStore}><App/></Provider>, document.getElementById('root'));
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
